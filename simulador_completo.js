7ece63cb76b7df4e4bc80d431499537fbe3647a7
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-29 20:26:53 -0500

    feat: funcion para ocultar todas las secciones

--- simulador_completo.js
+++ simulador_completo.js
@@ -1,13 +1,14 @@
+let clientes = [];
+let creditos = [];
 
-  let clientes = [];
-  let creditos = [];
+let tasaInteres = 15;
+let clienteSeleccionado = null;
+let cuotaCalculada = 0;
+let montoCalculado = 0;
+let plazoCalculado = 0;
+let creditoAprobado = false;
 
-  let tasaInteres = 15;
-  let clienteSeleccionado = null;
-  let cuotaCalculada = 0;
-  let montoCalculado = 0;
-  let plazoCalculado = 0;
-  let creditoAprobado = false;
-
-  
-//Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
+function ocultarSecciones() {
+  document.getElementById('parametros').classList.remove('activa');
+  document.getElementById('clientes').classList.remove('activa');
+}
