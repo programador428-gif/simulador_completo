b308b6c56e65f1bc214ecaaba9b195ba6c68ea22
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-29 20:13:07 -0500

    feat: archivos base

---
+++ simulador_completo.js
@@ -0,0 +1,13 @@
+
+  let clientes = [];
+  let creditos = [];
+
+  let tasaInteres = 15;
+  let clienteSeleccionado = null;
+  let cuotaCalculada = 0;
+  let montoCalculado = 0;
+  let plazoCalculado = 0;
+  let creditoAprobado = false;
+
+  
+//Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
